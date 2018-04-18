8ba0c413367f9d5a6e2678ab76bec456df7ebd86
Riad Mahdid <r.mahdid13@gmail.com>
2018-04-18 13:28:10 -0400

    Adding some test and the basic template for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   	root 'static_pages#home'
- 	get  '/home', to: 'static_pages#home', as: 'homes'
+ 	get  '/home', to: 'static_pages#home', as: 'home'
   	get  '/help', to: 'static_pages#help', as: 'help'
   	get  '/about', to: 'static_pages#about', as: 'about'
+
+  	get '/signup', to: 'users#new', as: 'signup'
+  	get '/log_in', to: 'static_pages#home', as: 'login'
 end
